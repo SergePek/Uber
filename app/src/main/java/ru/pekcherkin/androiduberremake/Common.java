d7e74cb7a5ae3f7146464607eae992f2a38bb641
ТЕХПРОМ <pekchiorckin@mail.ru>
2020-09-19 23:18:33 +0300

    Initial commit

---
+++ app/src/main/java/ru/pekcherkin/androiduberremake/Common.java
@@ -0,0 +1,9 @@
+package ru.pekcherkin.androiduberremake;
+
+import ru.pekcherkin.androiduberremake.Model.DriverInfoModel;
+
+public class Common {
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+
+    public static DriverInfoModel currentUser;
+}
